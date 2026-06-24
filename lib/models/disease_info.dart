d1e216aa99d0b984218a41d1c4fb110fc87c8330
kiwifruit47 <victoria.dimitrovax@icloud.com>
2026-06-24 18:45:50 +0300

    disease info repo and model

---
+++ lib/models/disease_info.dart
@@ -0,0 +1,13 @@
+class DiseaseInfo {
+  final String plantName;
+  final String diseaseName;
+  final String description;
+  final List<String> treatments;
+
+  DiseaseInfo({
+    required this.plantName,
+    required this.diseaseName,
+    required this.description,
+    required this.treatments,
+  });
+}
---
+++ lib/repository/disease_repository.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+import '../models/disease_info.dart';
+
+class DiseaseRepository {
+  Map<String, dynamic>? _cache;
+
+  Future<void> load() async {
+    final jsonString = await rootBundle.loadString('assets/description.json');
+    _cache = jsonDecode(jsonString);
+  }
+
+  DiseaseInfo getInfo(String label) {
+    if (_cache == null) {
+      throw Exception("Disease info not loaded.");
+    }
+
+    var result = _cache![label];
+    if (result != null) {
+      return DiseaseInfo(
+        plantName: result['plant_name'],
+        diseaseName: result['disease_name'],
+        description: result['description'],
+        treatments: List<String>.from(result['treatments']),
+      );
+    } else {
+      throw Exception("Label not found: $label");
+    }
+  }
+}
